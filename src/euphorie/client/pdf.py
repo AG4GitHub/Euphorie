15f7e4359fc20fee9c4e0b1368476561f3154120
Wolfgang Thomas <thomas@syslab.com>
2017-10-12 10:20:54 +0200

    more style fixes for PDF

--- src/euphorie/client/pdf.py
+++ src/euphorie/client/pdf.py
@@ -34,6 +34,7 @@ class PdfView(grok.View):
     def render(self):
         context = self.context
         view_name = self.request.get('view', 'view')
+        self.request['called_by'] = 'pdf'
         view = context.restrictedTraverse(view_name)
         pdf = self.view_to_pdf(view)
 
